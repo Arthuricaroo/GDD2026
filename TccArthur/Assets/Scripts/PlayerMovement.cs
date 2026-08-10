1a8011d24e264ec61dc5ee0decab6c51c977bb72
Marcinsensei <moreira.marcio@escolar.ifrn.edu.br>
2026-08-10 18:11:13 -0300

    Revert "gosma funcio."
    This reverts commit f4423639ceaae3255c95e78db04936c3af1dfd10.

--- TccArthur/Assets/Scripts/PlayerMovement.cs
+++ TccArthur/Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,76 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float velocidade = 5f;
+    public float jumpForce = 10f;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private Animator anim;
-    private SpriteRenderer sr;
 
-    Vector2 movement;
+    private bool isGrounded;
+    private bool isFacingRight = true;
+
+    public float gravityCooldown = 0.2f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        // Movimento horizontal
+        float moveX = Input.GetAxisRaw("Horizontal");
 
-        anim.SetFloat("Speed", movement.magnitude);
+        rb.linearVelocity = new Vector2(
+            moveX * velocidade,
+            rb.linearVelocity.y
+        );
 
-        if (movement.x > 0)
-            sr.flipX = false;
+        // Verifica chão
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            groundLayer
+        );
 
-        if (movement.x < 0)
-            sr.flipX = true;
+        // Pulo
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(
+                rb.linearVelocity.x,
+                jumpForce
+            );
+        }
+
+        // Animação de corrida
+        anim.SetBool("isRunning", moveX != 0);
+
+        // Flip do personagem
+        if (moveX > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (moveX < 0 && isFacingRight)
+        {
+            Flip();
+        }
     }
 
-    void FixedUpdate()
+    void Flip()
     {
-        rb.MovePosition(
-            rb.position +
-            movement * speed * Time.fixedDeltaTime
+        isFacingRight = !isFacingRight;
+
+        transform.localScale = new Vector3(
+            transform.localScale.x * -1,
+            transform.localScale.y,
+            transform.localScale.z
         );
     }
 }
